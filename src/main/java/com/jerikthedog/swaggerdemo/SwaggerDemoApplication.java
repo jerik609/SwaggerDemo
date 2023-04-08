b121c18c8dacee5255f7404a117910902c6a88d3
Michal <117671671+jerik609@users.noreply.github.com>
2023-04-08 15:08:29 +0200

    the "global" swagger annotations

--- src/main/java/com/jerikthedog/swaggerdemo/SwaggerDemoApplication.java
+++ src/main/java/com/jerikthedog/swaggerdemo/SwaggerDemoApplication.java
@@ -1,13 +1,40 @@
 package com.jerikthedog.swaggerdemo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// modifies the swagger UI - documentation basically + some functional stuff
+// the main usefulness still eludes me, but ok
+@OpenAPIDefinition(
+		info = @Info(
+				title= "TodoItems API",
+				version = "0.0.1.CAKE"
+		),
+		servers = {
+				@Server(
+						url = "http://localhost:8080"
+				),
+				@Server(
+						url = "http://jerik-the-dog.wuf:1234"
+				),
+		},
+		security = @SecurityRequirement(
+				name = "require sweets",
+				scopes = {"candy", "lolly"}
+		)
+)
 public class SwaggerDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SwaggerDemoApplication.class, args);
 	}
+
+
+
 
 }
